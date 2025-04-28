c9a7e42a1edef045f6c058ec78b179e93a967d97
Mahmoud_Shawqat <mahmoud.k.shawqat@gmail.com>
2025-04-28 12:10:05 +0300

    Add project files

---
+++ index.js
@@ -0,0 +1,31 @@
+let saveEl      = document.getElementById("save-el")
+let countEl     = document.getElementById("count-el")
+let countReset  = document.getElementById("reset-btn") 
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function dicrement() {
+    if(count){
+        count -= 1
+        countEl.textContent = count
+    }
+}
+
+function save() {
+    if(count){
+        let countStr = count + " - "
+        saveEl.textContent += countStr
+    }
+    countEl.textContent = 0
+    count = 0
+}
+
+function reset() {
+    saveEl.textContent  = "Previous entries: "
+    countEl.textContent = 0
+    count = 0
+}
